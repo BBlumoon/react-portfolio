184e0d0a7adb854993f815ac725cbe242bc10fd4
Colin Moon <blumoon1004@gmail.com>
2023-09-18 19:59:32 -0400

    finished navbar and made it reactive

--- my-react-portfolio/src/components/Navbar.js
+++ my-react-portfolio/src/components/Navbar.js
@@ -1,18 +1,29 @@
-import React from "react";
-import {Link} from "react-router-dom"
+import React, {useEffect, useState} from "react";
+import { Link, useLocation } from "react-router-dom"
+import '../styles/navbar.css'
+import ReorderIcon from '@mui/icons-material/Reorder'
 
 function Navbar() {
-    return <div className="navbar">
+    const [expandNavbar, setExpandNavbar] = useState(false)
+
+    const location = useLocation()
+    useEffect(() => {
+        setExpandNavbar(false)
+    }, [location]) 
+
+    return <div className="navbar" id={expandNavbar ? 'open' : 'close'}>
         <div className="toggleButton">
-            <button></button>
-            </div>
-            <div className="links">
-                <Link to='/'> Home </Link>
-                <Link to='/projects'> Projects </Link>
-                <Link to='/about'> About Me </Link>
-                <Link to='/contact'> Contact Me </Link>
-            </div>
+            <button onClick={() => {setExpandNavbar((prev) => !prev)}}>
+                <ReorderIcon/>
+            </button>
         </div>
+        <div className="links">
+            <Link to='/'> Home </Link>
+            <Link to='/projects'> Projects </Link>
+            <Link to='/about'> About Me </Link>
+            <Link to='/contact'> Contact Me </Link>
+        </div>
+    </div>
 }
 
 export default Navbar
